690169b21b4ad8de306d8422125c0c12e0f2f107
Umesh Dhangare <umeshiitbombay@gmail.com>
2023-04-30 01:07:52 +0530

    Cookies

--- app.js
+++ app.js
@@ -34,30 +34,38 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 // Basic Authentication
 var auth = (req, res, next) => {
-  console.log(req.headers);
+  console.log(req.signedCookies);
 
-  var authHeader = req.headers.authorization;
-  if (!authHeader) {
-    var err = new Error('You are not authenticated!');
-    err.status = 401;
-    return next(err);
-  }
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+      var err = new Error('You are not authenticated!');
+      err.status = 401;
+      return next(err);
+    }
 
-  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var user = auth[0];
-  var pass = auth[1];
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
 
-  if (user === 'admin' && pass === 'password') {
-    next(); // authorized
+    if (user === 'admin' && pass === 'password') {
+      res.cookie('user', 'admin', { signed: true, resave: true });
+      next(); // authorized
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 401;
+      return next(err);
+    }
   }
   else {
-    var err = new Error('You are not authenticated!');
-    err.status = 401;
-    return next(err);
+    if (req.signedCookies.user === 'admin') {
+      next();
+    }
   }
 }
 
