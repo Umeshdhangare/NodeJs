42e0999e51e44f97c6d127b2a8507fc710e858f9
Umesh <dhangareumesh@gmail.com>
2024-02-25 09:38:35 +0530

    Modifications

--- app.js
+++ app.js
@@ -1,45 +1,40 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var session = require('express-session');
-var FileStore = require('session-file-store')(session);
-var passport = require('passport');
-var authenticate = require('./authenticate');
-var config = require('./config');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
+var passport = require("passport");
+var authenticate = require("./authenticate");
+var config = require("./config");
 
 // Routes
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var dishRouter = require('./routes/dishRouter');
-var promoRouter = require('./routes/promoRouter');
-var leaderRouter = require('./routes/leaderRouter');
-var uploadRouter = require('./routes/uploadRouter');
-var favoriteRouter = require('./routes/favoritesRouter');
-var commentRouter = require('./routes/commentRouter');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var dishRouter = require("./routes/dishRouter");
+var promoRouter = require("./routes/promoRouter");
+var leaderRouter = require("./routes/leaderRouter");
+var uploadRouter = require("./routes/uploadRouter");
+var favoriteRouter = require("./routes/favoritesRouter");
+var commentRouter = require("./routes/commentRouter");
 
 // Models
-const Dishes = require('./models/dishes');
-const Promotions = require('./models/promotions');
-const Leaders = require('./models/leaders');
+const Dishes = require("./models/dishes");
+const Promotions = require("./models/promotions");
+const Leaders = require("./models/leaders");
 
 // MongoDB Connection
-const url = config.mongoUrl;
-const connect = mongoose.connect(url);
 
-connect.then((db) => {
-  console.log("Connected Succesfully to the server");
-}, (err) => { console.log(err); });
+config.connectDB();
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-09876-54321'));
@@ -47,32 +42,32 @@ app.use(express.urlencoded({ extended: false }));
 // Passport authentication
 passport.use(passport.initialize());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/dishes', dishRouter);
-app.use('/leaders', leaderRouter);
-app.use('/promotions', promoRouter);
-app.use('/imageUpload', uploadRouter);
-app.use('/favorites', favoriteRouter);
-app.use('/comments', commentRouter);
+app.use("/dishes", dishRouter);
+app.use("/leaders", leaderRouter);
+app.use("/promotions", promoRouter);
+app.use("/imageUpload", uploadRouter);
+app.use("/favorites", favoriteRouter);
+app.use("/comments", commentRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
 
 module.exports = app;
--- config.js
+++ config.js
@@ -1,4 +1,20 @@
+const mongoose = require("mongoose");
+
+const secretKey = "12345-67890-09876-54321";
+const mongoUrl =
+	"mongodb+srv://dhangareumesh:Umesh@1234@cluster0.m1eihh8.mongodb.net/?retryWrites=true&w=majority";
+
+const connectDB = async () => {
+	try {
+		const con = await mongoose.connect(mongoUrl);
+		console.log("DB connected successfully!");
+	} catch (err) {
+		console.log(`Error: ${err}`);
+		process.exit(1);
+	}
+};
+
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+	secretKey,
+	connectDB,
 };
